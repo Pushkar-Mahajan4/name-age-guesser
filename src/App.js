362afd49093e089f69d0ea9772db48c53976595b
Cassan0va27 <pushkarmahajan4@gmail.com>
2022-11-29 14:40:31 -0500

    First-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useCallback } from "react";
+import { FixedSizeList } from "react-window";
+import Header from "./components/header-files/Header";
 
 function App() {
+  const [name, setName] = useState("");
+  const [oddguess, setOddGuess] = useState(false);
+  const [guess, setGuess] = useState([]);
+
+  const fetchName = async () => {
+    setOddGuess(false);
+
+    if (name.trim() && name.trim() !== "") {
+      try {
+        const result = await fetch(`https://api.agify.io/?name=${name}`);
+        const user = await result.json();
+        if (user.age % 2 !== 0) {
+          setOddGuess(true);
+        }
+        setGuess([...guess, user]);
+        setName("");
+      } catch (e) {
+        e.printStackTrace();
+      }
+    } else {
+      alert("Invalid input");
+    }
+  };
+
+  const Row = useCallback(
+    ({ index, style }) => {
+      const { name, age } = guess[index];
+      return (
+        <div style={style}>
+          <div className="list-element">{`${name} - ${age}`}</div>
+        </div>
+      );
+    },
+    [guess]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <div className="card-body">
+          <h3 className="card-heading">Total Guesses : {guess.length}</h3>
+          <label className="odd-label">
+            {oddguess && "What an odd number of guess!"}
+          </label>
+          <div className="card-main">
+            <fieldset>
+              <legend>Please enter a name here</legend>
+              <input
+                type="text"
+                className="name-box"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              ></input>
+            </fieldset>
+            <div className="card-button">
+              <button className="button-clear" onClick={() => setName("")}>
+                Clear
+              </button>
+              <button className="button-submit" onClick={fetchName}>
+                Submit
+              </button>
+            </div>
+            <div className="card-list">
+              <h4>All the Guesses: </h4>
+              {guess.length !== 0 ? (
+                <div className="card-sublist">
+                  <FixedSizeList
+                    className="list-size"
+                    height={225}
+                    itemCount={guess.length}
+                    itemSize={50}
+                    width={350}
+                  >
+                    {Row}
+                  </FixedSizeList>
+                </div>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
